6068530923fccca2ac51a0cebf9acd45316057b4
Kailey Ferger <kaileyferger@wifi-10-43-15-30.wifi.berkeley.edu>
2023-12-05 16:52:42 -0800

    added tournament selection

--- genetic_algorithm/final_project_pseudo.py
+++ genetic_algorithm/final_project_pseudo.py
@@ -8,6 +8,7 @@ from sklearn.linear_model import LinearRegression
 import statsmodels.api as sm
 import pandas as pd
 import os
+import random
 
 def initialize_population(population_size, chromosome_length):
     """
@@ -21,20 +22,42 @@ def initialize_population(population_size, chromosome_length):
     - list: A list of random chromosomes.
     """
     population = [generate_random_chromosome(chromosome_length) for _ in range(population_size)]
+
     return population
 
 
-def generate_random_chromosome(chromosome_length):
+
+def adjust_chromosome(chromosome, max_features):
+    #function to adjust number of features in a chromsome to match max_features
+    current_sum = sum(chromosome)
+
+    if current_sum <= max_features:
+        return chromosome
+
+    ones_indices = [i for i, value in enumerate(chromosome) if value == 1]
+    indices_to_change = random.sample(ones_indices, current_sum - max_features)
+    
+    for index in indices_to_change:
+        chromosome[index] = 0
+
+    return chromosome
+
+
+def generate_random_chromosome(chromosome_length, max_features):
     """
     Generate a random binary chromosome
 
     Parameters: 
     - chromosome_length (int): The length of the chromosome.
+    - max_features (int): The maximum number of allowable features to include.
 
     Returns:
     - list: A list representing the random chromosome with 0s and 1s.
     """
     chromosome = np.random.randint(2, size=chromosome_length).tolist()
+    #ensure that the new chromsome doesn't exceed the max allowable features
+    chromosome = adjust_chromosome(chromosome, max_features)
+
     return chromosome
 
 def calculate_fitness(chromosome, data, outcome, objective_function="AIC"):
@@ -114,18 +137,30 @@ def calculate_rank_based_fitness(data, outcome, population, population_size, obj
 	p = population_size
 	return [(2*r)/(p*(p+1)) for r in fitness_ranks]
 
-def select_parents(population):
+def select_tournament_winners(population, winners_per_subgroup):
 	# implementing tournament selection
 	# one parent selected with probability proportional to its fitness
 	# other parent selected randomly
 
-	fitness_scores = [calculate_fitness(chromosome, population_size, data, outcome) for chromosome in population]  #weights to be used for parent selection
-	parent_indices = range(len(population))
-	parent1_index = random.choices(parent_indices, weights = fitness_scores, k=1) #choosing 1 parent with probability based on fitness values
-	parent1 = population[parent1_index]
-	parent2 = random.choices(population, k=1) #randomly choosing other parent 
+    winners = []
+    indexes_to_remove = []
+    losers = []
+    #tournament is a fitness evaluation; winners_per_subgroup is the 
+    # number of allowable winners moving onto the next generation
+    fitness_scores = [calculate_fitness(chromosome, len(population), data, outcome) for chromosome in population]  #weights to be used for parent selection
+    #stores indexes of original popualtion, in descending order of fitness
+    sorted_indices = sorted(range(len(fitness_scores)), key=lambda k: fitness_scores[k], reverse=True)
+    
+    for i in range(winners_per_subgroup):
+        winners.append(population[sorted_indices[i]])
+        #keep track of winners to remove from rest of breeding pool
+        indexes_to_remove.append(sorted_indices[i])
 
-	return parent1, parent2
+    #keeping track of losing individuals for random mating later on
+    for i in indexes_to_remove:
+        losers.append(population[i])
+
+    return winners, losers
 
 def crossover(parent1, parent2, population_size):
 	#We should choose how many crossover events per offspring we want, defaulting to 1 for now
@@ -134,46 +169,94 @@ def crossover(parent1, parent2, population_size):
 	recombination_points = range(population_size - 1) #can't recombine at ends of chromosomes
 	recomb_location_off1 = random.choices(recombination_points, replace=True)
 
-	offspring_1 = parent1[:recomb_location_off1] + parent2[recomb_location_off1:]
-
+	offspring = parent1[:recomb_location_off1] + parent2[recomb_location_off1:]
+    
 	return offspring
 
-def mutate(chromosome, mutation_rate=0.01):
+def mutate(chromosome, mutation_rate, max_features):
+    ##This function can only be run after crossover() to ensure max number of features is not exceeded
 
-	# mutation == True with probability 0.01, False otherwise
-	mutation = [random.random() < mutation_rate for i in range(len(chromosome))]
+    # mutation == True with probability 0.01, False otherwise
+    mutation = [random.random() < mutation_rate for i in range(len(chromosome))]
 
-	# '0' to '1' mutation or '1' to '0' mutation if mutation == True
-	new_chromsome = [abs(chromosome[i] - 1) if mutation[i] else chromosome[i] for i in range(len(chromosome))]
+    # '0' to '1' mutation or '1' to '0' mutation if mutation == True
+    new_chromsome = [abs(chromosome[i] - 1) if mutation[i] else chromosome[i] for i in range(len(chromosome))]
 
-	return chromosome
+    #ensure chromosome doesn't exceed max_features
+    #this checks for too many features gained from both crossover and mutation
+    new_chromosome = adjust_chromosome(new_chromosome, max_features)
 
-def genetic_algorithm(data,population_size=20, chromosome_length=27, generations=100, mutation_rate=0.01):
+    return new_chromsome
 
-	population = initialize_population(population_size, chromosome_length)
+def genetic_algorithm(data,population_size=20, chromosome_length=27, generations=100, mutation_rate=0.01, max_features=10):
 
-	for g in range(generations): #main iteration
-		rank_fitness_scores = calculate_rank_based_fitness(population, chromosome, population_size, data, outcome)
+    population = initialize_population(population_size, chromosome_length, max_features)
 
-		new_population = []
-		for i in range(population_size // 2):
-			parent1, parent2 = select_parents(population) #should this be done with or without replacement?
-			
-			#children are generated from indepdendent crossover events
-			child1 = crossover(parent1, parent2)
-			child2 = crossover(parent1, parent2)
+    for g in range(generations): #main iteration
+        # rank_fitness_scores = calculate_rank_based_fitness(population, chromosome, population_size, data, outcome)
 
-			#mutation
-			child1 = mutate(child1, mutation_rate)
-			child2 = mutate(child2, mutation_rate)
+        new_population = []
+        pop_subgroups = []
+        set_size = 5 #arbitrary starting value 
+        num_sets = len(population) / set_size
+        population_copy = population[:] #make a copy to keep track of removed individuals
 
-			new_population.add(child1, child2)
+        #random population subgroups for tournament selection
+        for _ in range(num_sets):
+             rand_sample = random.sample(population_copy, set_size)
+             pop_subgroups.append(rand_sample)
 
-		population = new_population #non-overlapping generations
+             #remove sampled individuals from original parent population
+             population_copy = [element for element in population_copy if element not in rand_sample]
+
+        #tournament selection- choose top n individuals from each subpopulation to 
+        #become parents, partnered with a random individual
+        all_winners = []
+        all_losers = []
+        for group in pop_subgroups:
+            winners, losers = select_tournament_winners(group, 1) #selecting 1 winner per subgroup
+            all_winners.extend(winners)
+            all_losers.extend(losers)
+
+        #parent selection and child generation
+        for ind in all_winners:
+            parent1 = ind
+            parent2 = all_losers.pop(random.randrange(len(all_losers))) #removes parent2 from loser list and returns selected parent2
+            child1 = crossover(parent1, parent2)
+            child2 = crossover(parent1, parent2)
+            child3 = crossover(parent1, parent2)
+
+            #mutation
+            child1 = mutate(child1, mutation_rate, max_features)
+            child2 = mutate(child2, mutation_rate, max_features)
+            child3 = mutate(child2, mutation_rate, max_features)
+
+            new_population.append(child1)
+            new_population.append(child2)
+            new_population.append(child3)
+        
+        #reproduction between randomly selected leftover individuals 
+        for _ in range(population_size - len(new_population)): #how many more children we need to add from randoms to maintain pop size
+            #sampled with replacement to avoid running out of new parents 
+            parent1 = random.sample(all_losers, 1)
+            parent2 = random.sample(all_losers, 1)
+            child1 = crossover(parent1, parent2)
+            #mutation
+            child1 = mutate(child1, mutation_rate, max_features)
+            new_population.append(child1)
+
+
+        #some sort of check here to make sure len(new_population) == len(old_population)?
+
+        #some function to check if we should terminate early, ie. (new fitness scores - previous fitness scores) < threshold ?
+        #should this be determined from winner pool only?
+
+        population = new_population #non-overlapping generations
 
 		most_fit_individual = population[max(rank_fitness_scores)]
 
 	return most_fit_individual
+
 
 class Generation_Container:
     def __init__(self):
@@ -223,6 +306,7 @@ fitness_scores = [calculate_fitness(chromosome, df, outcome, objective_function=
 
 # calculate ranked fitness scores for each objective function option
 ranked_fitness_scores = [calculate_rank_based_fitness(data, outcome, population, population_size, objective_function="AIC") for f in f_list]
+
 
 
 
