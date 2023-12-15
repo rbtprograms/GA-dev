b3455e20f854c888231cb28784ffd33e60918d82
Robert Thompson <rbt61690@gmail.com>
2023-12-15 11:03:42 -0800

    adding to the git ignore

--- baseball_example.py
+++ baseball_example.py
@@ -9,6 +9,5 @@ data = pd.read_csv(file_path, delimiter=' ')
 
 print(select(data, population_size=20, chromosome_length=27, generations=100, mutation_rate=0.01, max_features=27, 
                       outcome_index=0, objective_function="AIC", log_outcome=True, output_all_generation_data=True,
-                      with_elitism=True, plot_all_generation_data=True,plot_output_path='/Users/robertthompson/code/robert-thompson/GA-dev/examples',
-                      with_progress_bar=True))
+                      with_elitism=True,with_progress_bar=True))
 
